2ecb9ea1479e45f360443106f32a29b2796c4305
dingdongg <cs.dg.kim@gmail.com>
2024-04-29 14:12:40 -0600

    init svelte project + tailwind; basic UI boilerplate

---
+++ src/lib/Stat.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let name;
+    export const NAME = name;
+
+    let points = 0;
+
+    /**
+     * @param {number} value
+     */
+    function updatePoints(value) {
+        points = Math.min(255, points + value);
+    }
+
+    export function getPoints() {
+        return points;
+    }
+</script>
+
+<div class="flex flex-col m-4 text-2xl text-center">
+    <p>{name}</p>
+    <p>{points}</p>
+    <button class="bg-slate-500 p-3 my-3" on:click={() => updatePoints(1)}>+ 1</button>
+    <button class="bg-slate-600 p-3 my-3" on:click={() => updatePoints(2)}>+ 2</button>
+    <button class="bg-slate-700 p-3 my-3" on:click={() => updatePoints(3)}>+ 3</button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+    import Stat from "$lib/Stat.svelte";
+
+    /**
+     * @type {Stat}
+     */
+    let hpStat;
+    /**
+     * @type {Stat}
+     */
+    let atkStat;
+    /**
+     * @type {Stat}
+     */
+    let defStat;
+    /**
+     * @type {Stat}
+     */
+    let spaStat;
+    /**
+     * @type {Stat}
+     */
+    let spdStat;
+    /**
+     * @type {Stat}
+     */
+    let speStat;
+
+    async function saveToDB() {
+        console.log("SAVING to DB");
+
+        console.log("STATS:");
+        const stats = [hpStat, atkStat, defStat, spaStat, spdStat, speStat];
+
+        for (const stat of stats) {
+            console.log(stat.NAME, stat.getPoints());
+        }
+
+        const res = await fetch("http://localhost:8080/bar", {
+            headers: {
+                
+            }
+        });
+        console.log(res);
+    }
+
+</script>
+
+<h1>POKEMON EV TRACKER</h1>
+
+
+<div class="m-4 flex p-5 justify-around">
+    <Stat name='HP' bind:this={hpStat}></Stat>
+    <Stat name='ATTACK' bind:this={atkStat}></Stat>
+    <Stat name='DEFENSE' bind:this={defStat}></Stat>
+    <Stat name='SP. ATTACK' bind:this={spaStat}></Stat>
+    <Stat name='SP. DEFENSE' bind:this={spdStat}></Stat>
+    <Stat name='SPEED' bind:this={speStat}></Stat>
+</div>
+
+<button class="m-4 bg-green-800 p-4" on:click={saveToDB}>save to database</button>
+
