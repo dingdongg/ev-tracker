f3b4c872e73867a7f4caad3bb29f19f14a272b17
dingdongg <cs.dg.kim@gmail.com>
2024-04-30 15:29:50 -0600

    create a recently defeated pokemon component

---
+++ src/lib/RecentlyDefeated.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let pokemon;
+
+</script>
+
+<li class="m-3">
+    <img src={pokemon.image} alt={pokemon.name} fetchpriority="high">
+    <div class="flex justify-between">
+        <p>{pokemon.hp}</p>
+        <p>{pokemon.attack}</p>
+        <p>{pokemon.defense}</p>
+        <p>{pokemon["special-attack"]}</p>
+        <p>{pokemon["special-defense"]}</p>
+        <p>{pokemon.speed}</p>
+    </div>
+</li>
