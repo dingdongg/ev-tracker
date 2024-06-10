ab1ada30e6feb0a99ba6a8b76c1e97a1e27d205c
dingdongg <cs.dg.kim@gmail.com>
2024-06-09 22:59:17 -0600

    create login page

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,4 +1,14 @@
+import { redirect } from "@sveltejs/kit";
 import { signIn } from "../../auth";
 
 /** @type {import("./$types").Actions} */
 export const actions = { default: signIn };
+
+/** @type {import("./$types").PageServerLoad} */
+export async function load({ parent }) {
+    const { session } = await parent();
+
+    if (session) {
+        redirect(302, "/"); // if authenticated, redirect from /login to main page
+    }
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,26 @@
 <script>
-    import OAuth from "$lib/OAuth.svelte";
+    import { SignIn } from "@auth/sveltekit/components";
+    import * as Accordion from "$lib/components/ui/accordion";
 </script>
 
 <div>login type shiii</div>
 
-<OAuth provider="github" />
+<div class="bg-zinc-600 min-w-[40vw] flex flex-col items-center py-20">
+    <p>Log In</p>
+    <SignIn provider="github" signInPage="login" options={{ redirectTo: "/" }}>
+        <span slot="submitButton">Sign in with Github</span>
+    </SignIn>
+</div>
+<Accordion.Root class="min-w-[40vw] max-w-[60vw]">
+    <Accordion.Item value="item-1">
+        <Accordion.Trigger>Why should I make an account?</Accordion.Trigger>
+        <Accordion.Content>
+            <p>
+                If all you want to do is read your savefile, you don't need an account.
+            </p>
+            <p>
+                However, if you want to modify your savefile, you need to sign up!
+            </p>
+        </Accordion.Content>
+        </Accordion.Item>
+</Accordion.Root>
