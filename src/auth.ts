b67dd355fa4c2d2bcf333f1d08993ab459acbefe
dingdongg <cs.dg.kim@gmail.com>
2024-06-11 18:05:24 -0600

    separate dev and prod environments

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,30 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Github from "@auth/sveltekit/providers/github";
-import { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET, AUTH_SECRET } from "$env/static/private";
+import { 
+    AUTH_GITHUB_ID, 
+    AUTH_GITHUB_SECRET, 
+    AUTH_SECRET, 
+    DEV_AUTH_GITHUB_ID,
+    DEV_AUTH_GITHUB_SECRET,
+    DEV_AUTH_SECRET,
+} from "$env/static/private";
 
-export const { handle, signIn, signOut } = SvelteKitAuth({
-    providers: [
-        Github({
-            clientId: AUTH_GITHUB_ID,
-            clientSecret: AUTH_GITHUB_SECRET,
-        }),
-    ],
-    trustHost: true,
-    secret: AUTH_SECRET,
-});
+const githubConfig = (clientId: string, clientSecret: string, encryptionToken: string) => {
+    return {
+        providers: [
+            Github({ clientId, clientSecret }),
+        ],
+        trustHost: true,
+        secret: encryptionToken,
+    };
+};
+
+let config;
+
+if (import.meta.env.DEV) {
+    config = githubConfig(DEV_AUTH_GITHUB_ID, DEV_AUTH_GITHUB_SECRET, DEV_AUTH_SECRET);
+} else {
+    config = githubConfig(AUTH_GITHUB_ID, AUTH_GITHUB_SECRET, AUTH_SECRET);
+}
+
+export const { handle, signIn, signOut } = SvelteKitAuth(config);
