980332ac866f9bcdbb3ac44bc17a4db073db2c20
dingdongg <cs.dg.kim@gmail.com>
2024-06-11 23:57:42 -0600

    include user ID in /update endpoint

--- src/routes/api/update/+server.js
+++ src/routes/api/update/+server.js
@@ -2,6 +2,7 @@ import { PRIVATE_API_KEY, PRIVATE_BACKEND_URL } from "$env/static/private";
 
 /** @type {import("./$types").RequestHandler} */
 export async function POST(event) {
+    // return new Response(null, { status: 403 });
     const session = await event.locals.auth();
     let sessionExpired = session?.expires && new Date() > new Date(session.expires);
 
@@ -10,11 +11,15 @@ export async function POST(event) {
     }
     
     const body = await event.request.json();
+    const payload = {
+        requests: body,
+        userId: session.user.id,
+    };
 
     // TODO: change URL back to private backend url
-    const res = await fetch(`${PRIVATE_BACKEND_URL}/save`, {
+    const res = await fetch(`${"http://localhost:8080"}/save`, {
         method: "POST",
-        body: JSON.stringify(body),
+        body: JSON.stringify(payload),
         headers: {
             "x-api-key": PRIVATE_API_KEY,
             "Content-Type": "application/json",
