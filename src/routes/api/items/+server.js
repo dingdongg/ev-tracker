24d979495e2a47902fc305b6a45543b5009f6382
dingdongg <cs.dg.kim@gmail.com>
2024-06-14 02:17:33 -0600

    create /items endpoint

---
+++ src/routes/api/items/+server.js
@@ -0,0 +1,18 @@
+import { PRIVATE_API_KEY } from "$env/static/private";
+import { get } from "$lib/server/api";
+
+/** @type {import("./$types").RequestHandler} */
+export async function GET(event) {
+    const res = await get("/items", {
+        "x-api-key": PRIVATE_API_KEY,
+        "Content-Type": "application/json",
+    });
+
+    return new Response(res.body, {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "cache-control": "public, max-age=3600",
+        },
+    });
+}
